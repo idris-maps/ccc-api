8e16edb134672e2501cd2a761c90cbf8def82da5
Idris <idris.maps@gmx.ch>
2017-04-19 07:59:59 +0200

    sqlite db

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
+var models = require('./lib/models')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 var port = +process.env.PORT || 3000
-app.listen(port, function() {
-	console.log('Server started on port ' + port)
+var dev = true
+if(process.env.DEV === 'false') { dev = false }
+
+models.init(dev, function() {
+	app.listen(port, function() {
+		console.log('Server started on port ' + port)
+	})
 })
+
+
+
