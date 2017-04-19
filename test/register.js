89bafd4c7e408e7f4e115bb5ec565eeb4f424516
Idris <idris.maps@gmx.ch>
2017-04-19 07:47:49 +0200

    test setup

---
+++ test/register.js
@@ -0,0 +1,9 @@
+var request = require('supertest')
+var app = require('../server')
+
+describe('POST /register', function() {
+	it('should reject empty email')
+	it('should reject invalid email')
+	it('should reject empty password')
+	it('should return id')
+})
