a1bb5a951898c4392af29d416b79203cdc35926e
Idris <idris.maps@gmx.ch>
2017-04-19 08:41:23 +0200

    /register rejects existing email

--- test/register.js
+++ test/register.js
@@ -4,7 +4,12 @@ var app = require('../server')
 describe('POST /register', function() {
 	before(function(done) {
 		app.on('ready', function() {
-			done()
+			var User = require('../lib/models').model().user
+			User.create({
+				email: 'existing@mail.com', password: 'password'
+			}).then(function() {
+				done()
+			})
 		})
 	})
 	it('should reject empty email', function(done) {
@@ -28,6 +33,14 @@ describe('POST /register', function() {
 			.post('/register')
 			.type('json')
 			.send({ email: 'is@email.com'})
+			.expect(400)
+			.end(done)
+	})
+	it('should reject existing email', function(done) {
+		request(app)
+			.post('/register')
+			.type('json')
+			.send({ email: 'existing@mail.com', password: 'password'})
 			.expect(400)
 			.end(done)
 	})
