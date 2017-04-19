b49580de97b378253ddf874e35e18719548466ac
Idris <idris.maps@gmx.ch>
2017-04-19 08:26:51 +0200

    struggling with test. works with postman...

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
 var models = require('./lib/models')
+var routes = require('./lib/routes')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
+routes(app)
 
 var port = +process.env.PORT || 3000
 var dev = true
@@ -14,8 +16,11 @@ if(process.env.DEV === 'false') { dev = false }
 models.init(dev, function() {
 	app.listen(port, function() {
 		console.log('Server started on port ' + port)
+		app.emit('ready')
 	})
 })
+
+module.exports = app
 
 
 
--- test/register.js
+++ test/register.js
@@ -2,7 +2,14 @@ var request = require('supertest')
 var app = require('../server')
 
 describe('POST /register', function() {
+	before(function(done) {
+		app.on('ready', function() {
+			console.log('Test can start')
+			done()
+		})
+	})
 	it('should reject empty email', function(done) {
+		console.log('Runnning test')
 		request(app)
 			.post('/user')
 			.type('json')
