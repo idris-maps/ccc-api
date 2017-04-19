6de4f5221a3030dfdb89864f8d24f68088992517
Idris <idris.maps@gmx.ch>
2017-04-19 10:07:43 +0200

    POST /contract rejects if no token

--- test/uj2-add-contract.js
+++ test/uj2-add-contract.js
@@ -1,12 +1,22 @@
 var request = require('supertest')
 var app = require('../server')
 var hash = require('password-hash')
+var jwt = require('jsonwebtoken')
+var secret = require('../secret')
+var token
 
 describe('\"UJ2\" POST /contract', function() {
 	before(function(done) {
 		prepare(function() { done() })
 	})
-	it('should reject if no token')
+	it('should reject if no token', function(done) {
+		request(app)
+			.post('/contract')
+			.type('json')
+			.send({ title: 'contract-4', company: 'X', price: 120 })
+			.expect(400)
+			.end(done)
+	})
 	it('should reject empty title')
 	it('should reject empty company')
 	it('should reject empty price')
@@ -18,7 +28,10 @@ function prepare(callback) {
 	var User = require('../lib/models').model().user
 	User.truncate().then(function() {
 		User.create({ email: 'existing@email.com', password: hash.generate('password') })
-		.then(function() { callback() })
+		.then(function(user) { 
+			token = jwt.sign({id: user.dataValues.id}, secret.jwt, { expiresIn: 10000 })
+			callback() 
+		})
 		.catch(function(err) { console.log('Error creating User', err) })
 	})
 }
